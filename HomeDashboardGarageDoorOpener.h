1daed803792463385384661750af9178ddc7a20b
Daniel Kis <kisdaniel@gmail.com>
2018-09-01 12:00:23 +0200

    Initial import

---
+++ HomeDashboardGarageDoorOpener.h
@@ -0,0 +1,29 @@
+/* GarageDoor-Opener.h */ 
+#ifndef GARAGEDOOR_OPENER_H
+#define GARAGEDOOR_OPENER_H
+
+#define SERIAL_BAUD 115200
+
+// Pin layout
+// D3 is reserved during programming
+
+#define OPEN_MOTOR_PIN      D5    // The pin that activates the open door motor
+#define CLOSE_MOTOR_PIN     D6  // The pin that activates the close door motor 
+#define OPEN_SWITCH_PIN     D7
+#define CLOSE_SWITCH_PIN    D8
+#define LIGHT_SWITCH_PIN    D4  // The pin that turns the light on / off
+
+#define GARAGE_DOOR_MQTT_CLIENT_ID "garage-door"
+
+#include <Arduino.h>
+
+typedef enum {
+    GD_OPEN,
+    GD_CLOSED,
+    GD_OPENING,
+    GD_CLOSING,
+    GD_PARTIALLY_OPENED,
+    GD_UNKNOWN
+} GarageDoorState;
+
+#endif
---
+++ HomeDashboardGarageDoorOpener.ino
@@ -0,0 +1,574 @@
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include "HomeDashboardGarageDoorOpener.h"
+
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+
+#include <PubSubClient.h>
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char mqtt_server[40];
+char mqtt_port[6] = "1883";
+char device_name[34] = "GarageDoorOpener";
+
+char inTopic[60];
+char outTopic[60];
+
+//flag for saving data
+bool mqttFailedBefore = false;
+
+DynamicJsonBuffer jsonBuffer(512);
+
+WiFiManager wifiManager;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+WiFiManagerParameter custom_device_name("deviceName", "Device name", device_name, 32);
+
+GarageDoorState currentState = GD_UNKNOWN;
+GarageDoorState previousState = GD_UNKNOWN;
+boolean previousOpenPressed = false;
+boolean previousClosePressed = false;
+
+boolean lampUserRequestedState = false;
+
+long lastConnectRetry = 0;
+long keeplightOnUntil = 0;
+
+long keeplightOnAfterOpeningInMillisec = 20000;
+long keeplightOnAfterClosingInMillisec = 30000;
+
+long openAndClosingTimeout = 15000;
+
+long openState = 0;
+long lastStateChange = 0;
+
+long previousChangeInMillis = 0;
+long lastStateSent = 0;
+
+long disableLamp = 0;
+
+String getDoorStateAsString(GarageDoorState state)
+{
+  switch (state)
+  {
+  case GD_OPEN:
+    return String("open");
+  case GD_CLOSED:
+    return String("closed");
+  case GD_OPENING:
+    return String("opening");
+  case GD_CLOSING:
+    return String("closing");
+  case GD_PARTIALLY_OPENED:
+    return String("partiallyOpened");
+  case GD_UNKNOWN:
+    return String("unknown");
+  }
+
+  Serial.println("Garage Door State Value Unknown!");
+
+  return String("unknown");
+}
+
+void turnLight(boolean on)
+{
+  if (disableLamp)
+  {
+    digitalWrite(LIGHT_SWITCH_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LIGHT_SWITCH_PIN, on ? LOW : HIGH);
+  }
+}
+
+void lightLoop()
+{
+  if (keeplightOnUntil > 0)
+  {
+    long now = millis();
+    if (now > keeplightOnUntil && !lampUserRequestedState)
+    {
+      keeplightOnUntil = 0;
+      turnLight(false);
+    }
+  }
+}
+
+void open()
+{
+  if (currentState == GD_UNKNOWN)
+  {
+    openState = 0;
+  }
+  lastStateChange = millis();
+  stop(GD_PARTIALLY_OPENED);
+
+  digitalWrite(OPEN_MOTOR_PIN, LOW);
+  // digitalWrite(CLOSE_MOTOR_PIN, HIGH);
+  keeplightOnUntil = lastStateChange + keeplightOnAfterOpeningInMillisec;
+  turnLight(true);
+
+  currentState = GD_OPENING;
+
+  publishState();
+}
+
+void close()
+{
+  if (currentState != GD_CLOSING && currentState != GD_CLOSED)
+  {
+    if (currentState == GD_UNKNOWN)
+    {
+      openState = openAndClosingTimeout;
+    }
+    lastStateChange = millis();
+    stop(GD_PARTIALLY_OPENED);
+
+    // digitalWrite(OPEN_MOTOR_PIN, HIGH);
+    digitalWrite(CLOSE_MOTOR_PIN, LOW);
+
+    keeplightOnUntil = lastStateChange + keeplightOnAfterOpeningInMillisec;
+    turnLight(true);
+
+    currentState = GD_CLOSING;
+  }
+}
+
+void stop(GarageDoorState newState)
+{
+  if (currentState != newState)
+  {
+    digitalWrite(OPEN_MOTOR_PIN, HIGH);
+    digitalWrite(CLOSE_MOTOR_PIN, HIGH);
+    switch (currentState)
+    {
+    case GD_OPENING:
+      openState = openState + lastStateChange - millis();
+      keeplightOnUntil = millis() + keeplightOnAfterOpeningInMillisec;
+      if (openState > openAndClosingTimeout)
+      {
+        openState = openAndClosingTimeout;
+      }
+      break;
+    case GD_CLOSING:
+      openState = openState - (lastStateChange - millis());
+      if (openState < 0)
+      {
+        openState = 0;
+      }
+      keeplightOnUntil = millis() + keeplightOnAfterClosingInMillisec;
+      break;
+    default:
+      turnLight(lampUserRequestedState);
+      break;
+    }
+    if (currentState != newState)
+    {
+      currentState = newState;
+      publishState();
+    }
+  }
+}
+
+void calculateOpenStateAndStopIfNecessary()
+{
+  long now;
+  switch (currentState)
+  {
+  case GD_OPENING:
+    now = millis();
+    openState = openState + now - lastStateChange;
+    lastStateChange = now;
+    if (openState > openAndClosingTimeout)
+    {
+      stop(GD_OPEN);
+    }
+    else if (now - lastStateSent > 1000)
+    {
+      publishState();
+    }
+
+    break;
+  case GD_CLOSING:
+    now = millis();
+    openState = openState - now + lastStateChange;
+    lastStateChange = now;
+    if (openState < 0)
+    {
+      stop(GD_CLOSED);
+    }
+    else if (now - lastStateSent > 1000)
+    {
+      publishState();
+    }
+
+    break;
+  default:
+    break;
+  }
+}
+
+void checkButtonCommands()
+{
+  bool openPressed = digitalRead(OPEN_SWITCH_PIN) == HIGH;
+  bool closePressed = digitalRead(CLOSE_SWITCH_PIN) == HIGH;
+  long now = millis();
+  if ((previousOpenPressed != openPressed) || (previousClosePressed != closePressed))
+  {
+    if (previousChangeInMillis + 500 < now)
+    {
+      if (openPressed && closePressed)
+      {
+        Serial.println("open and close pressed");
+      }
+      else if (openPressed)
+      {
+        Serial.println("open pressed");
+        if (currentState == GD_OPENING)
+        {
+          stop(GD_PARTIALLY_OPENED);
+        }
+        else
+        {
+          open();
+        }
+      }
+      else if (closePressed)
+      {
+        Serial.println("close pressed");
+        if (currentState == GD_CLOSING)
+        {
+          stop(GD_PARTIALLY_OPENED);
+        }
+        else
+        {
+          close();
+        }
+      }
+      previousClosePressed = closePressed;
+      previousOpenPressed = openPressed;
+      previousChangeInMillis = now;
+    }
+  }
+}
+
+//callback notifying us of the need to save config
+void saveConfigCallback()
+{
+  Serial.println("saving config");
+
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(device_name, custom_device_name.getValue());
+
+  JsonObject &json = jsonBuffer.createObject();
+  json["mqtt_server"] = mqtt_server;
+  json["mqtt_port"] = mqtt_port;
+  json["device_name"] = device_name;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile)
+  {
+    Serial.println("failed to open config file for writing");
+  }
+  json.printTo(Serial);
+  json.printTo(configFile);
+  configFile.close();
+  jsonBuffer.clear();
+  //end save
+}
+
+void loadConfig()
+{
+  if (SPIFFS.begin())
+  {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json"))
+    {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile)
+      {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+        configFile.readBytes(buf.get(), size);
+        JsonObject &json = jsonBuffer.parseObject(buf.get());
+
+        json.printTo(Serial);
+
+        if (json.success())
+        {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(device_name, json["device_name"]);
+        }
+        else
+        {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+        jsonBuffer.clear();
+      }
+    }
+    else
+    {
+      Serial.println("No config file present");
+    }
+  }
+  else
+  {
+    Serial.println("failed to mount FS");
+  }
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  JsonObject &inputObject = jsonBuffer.parseObject(payload);
+  const char *command = inputObject["command"];
+  if (strcmp(command, "open") == 0)
+  {
+    open();
+  }
+  else if (strcmp(command, "close") == 0)
+  {
+    close();
+  }
+  else if (strcmp(command, "lighton") == 0)
+  {
+    keeplightOnUntil = atoi(inputObject["timeoutInMillis"]) + millis();
+    turnLight(true);
+  }
+  else if (strcmp(command, "lightoff") == 0)
+  {
+    turnLight(false);
+  }
+  else if (strcmp(command, "disableLight") == 0)
+  {
+    disableLamp = 1;
+    turnLight(false);
+  }
+  else if (strcmp(command, "enableLight") == 0)
+  {
+    disableLamp = 0;
+  }
+  else if (strcmp(command, "stop") == 0)
+  {
+    stop(GD_PARTIALLY_OPENED);
+  }
+  Serial.println(command);
+  jsonBuffer.clear();
+}
+
+void connectToMqttIfNotConnected()
+{
+  if (!client.connected())
+  {
+    long now = millis();
+    if (lastConnectRetry + 10000 < now)
+    {
+      client.setCallback(mqttCallback);
+      client.setServer(mqtt_server, atoi(mqtt_port));
+      if (client.connect(device_name))
+      {
+        lastConnectRetry = millis();
+        Serial.println("Connected to MQTT");
+        mqttFailedBefore = false;
+
+        JsonObject &json = jsonBuffer.createObject();
+        json["name"] = device_name;
+        json["type"] = "GarageDoorOpener";
+
+        char jsonChar[160];
+        json.printTo((char *)jsonChar, json.measureLength() + 1);
+
+        client.publish("/homedashboard/device/registration", jsonChar);
+        strcpy(inTopic, "/homedashboard/");
+        strcat(inTopic, device_name);
+        strcpy(outTopic, inTopic);
+        strcat(inTopic, "/in");
+        strcat(outTopic, "/out");
+
+        client.subscribe(inTopic);
+        Serial.print("Subscribe to ");
+        Serial.println(inTopic);
+        publishState();
+      }
+      else
+      {
+        lastConnectRetry = millis();
+        if (!mqttFailedBefore)
+        {
+          Serial.print("Failed to connect to MQTT server: ");
+          Serial.print(mqtt_server);
+          Serial.print(", port: ");
+          Serial.println(atoi(mqtt_port));
+        }
+        mqttFailedBefore = true;
+      }
+    }
+  }
+  else
+  {
+    client.loop();
+  }
+}
+
+void publishState()
+{
+  JsonObject &json = jsonBuffer.createObject();
+  json["name"] = device_name;
+  json["state"] = getDoorStateAsString(currentState);
+  char position[10];
+  itoa(openState, position, 10);
+  json["position"] = position;
+  json["lightDisabled"] = disableLamp ? "1" : "0";
+
+  char jsonChar[150];
+  json.printTo((char *)jsonChar, json.measureLength() + 1);
+
+  client.publish(outTopic, jsonChar);
+  // Serial.println(jsonChar);
+  // Serial.println(openState);
+
+  jsonBuffer.clear();
+
+  lastStateSent = millis();
+}
+
+void initPins()
+{
+  Serial.println("init pins");
+  pinMode(OPEN_MOTOR_PIN, OUTPUT);
+  pinMode(CLOSE_MOTOR_PIN, OUTPUT);
+  pinMode(LIGHT_SWITCH_PIN, OUTPUT);
+
+  pinMode(OPEN_SWITCH_PIN, INPUT);
+  pinMode(CLOSE_SWITCH_PIN, INPUT);
+
+  digitalWrite(OPEN_MOTOR_PIN, HIGH);
+  digitalWrite(CLOSE_MOTOR_PIN, HIGH);
+  digitalWrite(LIGHT_SWITCH_PIN, HIGH);
+
+  delay(2000);
+  bool openPressed = digitalRead(OPEN_SWITCH_PIN) == HIGH;
+  bool closePressed = digitalRead(CLOSE_SWITCH_PIN) == HIGH;
+  if (openPressed && closePressed)
+  {
+
+    Serial.println("open and close pressed...");
+    digitalWrite(LIGHT_SWITCH_PIN, LOW);
+    delay(1000);
+    digitalWrite(LIGHT_SWITCH_PIN, HIGH);
+
+    openPressed = digitalRead(OPEN_SWITCH_PIN) == HIGH;
+    closePressed = digitalRead(CLOSE_SWITCH_PIN) == HIGH;
+    if (openPressed && closePressed)
+    {
+      digitalWrite(LIGHT_SWITCH_PIN, LOW);
+      delay(200);
+      digitalWrite(LIGHT_SWITCH_PIN, HIGH);
+
+      Serial.println("reset requested...");
+      wifiManager.resetSettings();
+    }
+  }
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  Serial.println("starting...");
+
+  //clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+
+  loadConfig();
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  // set static ip
+  // wifiManager.setSTAStaticIPConfig(IPAddress(10, 0, 1, 99), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_device_name);
+
+  WiFi.hostname(device_name);
+  wifiManager.setConfigPortalTimeout(60 * 3);
+
+  initPins();
+
+  if (!wifiManager.autoConnect("HomeDashboardConfigAP", "password"))
+  {
+    Serial.println("failed to connect and hit timeout, you should reset to reconfigure");
+  }
+  else
+  {
+    Serial.println("connected...");
+    Serial.println("local ip");
+    Serial.println(WiFi.localIP());
+  }
+
+  //read updated parameters
+
+  //save the custom parameters to FS
+}
+
+void loop()
+{
+  if (!WiFi.isConnected() && currentState != GD_OPENING && currentState != GD_CLOSING)
+  {
+    Serial.println("Wifi connection lost...");
+    if (WiFi.reconnect())
+    {
+      Serial.print("successfully reconnected, local ip:");
+      Serial.println(WiFi.localIP());
+      connectToMqttIfNotConnected();
+    }
+  }
+  else
+  {
+    connectToMqttIfNotConnected();
+  }
+  checkButtonCommands();
+  lightLoop();
+  calculateOpenStateAndStopIfNecessary();
+}
